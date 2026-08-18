e5b20a2248cc40d0e69f7ed3bfd646fb40452fae
BeaMaB <beatrizmabu@gmail.com>
2026-08-18 16:35:35 +0100

    Updated search page layout to include product results

--- src/main/java/ci553/happyshop/client/customer/CustomerView.java
+++ src/main/java/ci553/happyshop/client/customer/CustomerView.java
@@ -197,6 +197,13 @@ public class CustomerView  {
                 }
             }
         });
+
+        // Combine everything into the VBox
+        VBox vbSearchPage = new VBox(15, laPageTitle, searchBox, hbBtn, hbSummary, vbSearchResult, obrLvProducts);
+        vbSearchPage.setPrefWidth(COLUMN_WIDTH);
+        vbSearchPage.setAlignment(Pos.TOP_CENTER);
+        vbSearchPage.setStyle("-fx-padding: 15px;");
+
         return vbSearchPage;
     }
 
