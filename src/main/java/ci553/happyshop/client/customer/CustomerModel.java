9b6c7ae3c3d6f29610768fccc386bf7b314a3429
BeaMaB <beatrizmabu@gmail.com>
2026-08-18 10:20:09 +0100

    Added product list to support multiple search results

--- src/main/java/ci553/happyshop/client/customer/CustomerModel.java
+++ src/main/java/ci553/happyshop/client/customer/CustomerModel.java
@@ -27,6 +27,7 @@ public class CustomerModel {
                                   //Benefits: Flexibility: Easily change the database implementation.
 
     private Product theProduct =null; // product found from search
+    private ArrayList<Product> productList = new ArrayList<>();
     private ArrayList<Product> trolley =  new ArrayList<>(); // a list of products in trolley
 
     // Four UI elements to be passed to CustomerView for display updates.
