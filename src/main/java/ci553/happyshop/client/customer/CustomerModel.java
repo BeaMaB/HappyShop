4777792ab4c9945b855732e3dac273eca0d2ef8b
BeaMaB <beatrizmabu@gmail.com>
2026-08-18 19:50:39 +0100

    Updated CustomerView with trolley product list

--- src/main/java/ci553/happyshop/client/customer/CustomerModel.java
+++ src/main/java/ci553/happyshop/client/customer/CustomerModel.java
@@ -231,7 +231,7 @@ public class CustomerModel {
         else{
             imageName = "imageHolder.jpg";
         }
-        cusView.update(imageName, displayLaSearchResult, productList, displayTaTrolley,displayTaReceipt);
+        cusView.update(imageName, displayLaSearchResult, productList, trolley, displayTaReceipt);
     }
      // extra notes:
      //Path.toUri(): Converts a Path object (a file or a directory path) to a URI object.
