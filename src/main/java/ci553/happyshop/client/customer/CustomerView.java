997888997eec0006ccf813350cccc18d1e7e4ec5
BeaMaB <beatrizmabu@gmail.com>
2026-08-18 19:44:00 +0100

    Added trolley total to the trolley page layout

--- src/main/java/ci553/happyshop/client/customer/CustomerView.java
+++ src/main/java/ci553/happyshop/client/customer/CustomerView.java
@@ -309,7 +309,7 @@ public class CustomerView  {
         hbBtns.setStyle("-fx-padding: 15px;");
         hbBtns.setAlignment(Pos.CENTER);
 
-        vbTrolleyPage = new VBox(15, laPageTitle, lvTrolley, hbBtns);
+        vbTrolleyPage = new VBox(15, laPageTitle, hbBtns, lvTrolley, lbTrolleyTotal);
         vbTrolleyPage.setPrefWidth(COLUMN_WIDTH);
         vbTrolleyPage.setAlignment(Pos.TOP_CENTER);
         vbTrolleyPage.setStyle("-fx-padding: 15px;");
