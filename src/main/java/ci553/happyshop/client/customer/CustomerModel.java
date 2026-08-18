0f7db50c2594efead45bfd913dcf75959d795ba5
BeaMaB <beatrizmabu@gmail.com>
2026-08-18 19:50:00 +0100

    Added trolley item quantity (increase and decrease) and removal controls to CustomerModel

--- src/main/java/ci553/happyshop/client/customer/CustomerModel.java
+++ src/main/java/ci553/happyshop/client/customer/CustomerModel.java
@@ -109,6 +109,37 @@ public class CustomerModel {
         updateView();
     }
 
+    public void increaseQuantity(Product product) {
+        if (product == null) return;
+
+        product.setOrderedQuantity(product.getOrderedQuantity() + 1);
+
+        displayTaTrolley = ProductListFormatter.buildString(trolley);
+        updateView();
+    }
+
+    public void decreaseQuantity(Product product) {
+        if (product == null) return;
+
+        if (product.getOrderedQuantity() > 1) {
+            product.setOrderedQuantity(product.getOrderedQuantity() - 1);
+        } else {
+            trolley.remove(product);
+        }
+
+        displayTaTrolley = ProductListFormatter.buildString(trolley);
+        updateView();
+    }
+
+    public void removeProduct(Product product) {
+        if (product == null) return;
+
+        trolley.remove(product);
+
+        displayTaTrolley = ProductListFormatter.buildString(trolley);
+        updateView();
+    }
+
     void checkOut() throws IOException, SQLException {
         if(!trolley.isEmpty()){
             // Group the products in the trolley by productId to optimize stock checking
