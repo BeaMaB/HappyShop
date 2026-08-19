144f1e38670e81b991c65684f3647ae668569fa9
BeaMaB <beatrizmabu@gmail.com>
2026-08-19 08:49:57 +0100

    Added background music playback with looping and volume control and uploaded audio file for SoundManager

--- src/main/java/ci553/happyshop/utility/SoundManager.java
+++ src/main/java/ci553/happyshop/utility/SoundManager.java
@@ -5,4 +5,21 @@ import javafx.scene.media.MediaPlayer;
 import java.net.URL;
 
 public class SoundManager {
+    public static MediaPlayer backgroundMusic;
+
+    // The method to play the background music
+    public static void playBackgroundMusic(String fileName) {
+        try {
+            // Using SoundManager.class instead of Main.class is CRITICAL
+            URL resource = SoundManager.class.getResource("/background_music.mp3");
+            if (resource != null) {
+                backgroundMusic = new MediaPlayer(new Media(resource.toExternalForm()));
+                backgroundMusic.setCycleCount(MediaPlayer.INDEFINITE);
+                backgroundMusic.setVolume(0.3);
+                backgroundMusic.play();
+            }
+        } catch (Exception e) {
+            System.out.println("Audio Error: " + e.getMessage());
+        }
+    }
 }
