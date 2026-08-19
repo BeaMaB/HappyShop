ae6f2022942847eb36350dfecd64bfbef3c9f59f
BeaMaB <beatrizmabu@gmail.com>
2026-08-19 08:47:47 +0100

    Added JavaFX Media module requirement for SoundManager audio functionality

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module ci553.happyshop {
     requires java.sql;
     requires javafx.graphics;
     requires javafx.base;
+    requires javafx.media;
 
     opens ci553.happyshop to javafx.fxml;
     opens ci553.happyshop.client to javafx.fxml;
